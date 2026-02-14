485110cd84580ef68d4b6fb8d1b3e562fc19dbcc
KachiAlex <opd.livmind@gmail.com>
2026-02-14 02:29:03 +0100

    test: export named Login from LoginNew for integration tests

--- src/pages/LoginNew.js
+++ src/pages/LoginNew.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const LoginNew = () => {
+export const Login = () => {
   return (
     <div>
       <h1>Login (stub)</h1>
+      <label htmlFor="email">Email</label>
+      <input id="email" name="email" placeholder="you@example.com" />
     </div>
   );
 };
+
+const LoginNew = Login;
 
 export default LoginNew;
